4fb7953142c99397573f8a68400678dc0ea6c8d4
Daniel Garrido Sánchez <gareda95@outlook.es>
2024-01-21 22:00:57 +0100

    terraform, CHANGELOG and README (#9)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+<!-- markdownlint-disable MD041 MD013 -->
 ## 1.1.0 (February 11, 2021)
 
 FEATURES:
--- output.tf
+++ output.tf
@@ -2,35 +2,43 @@ output "id" {
   value       = azurerm_key_vault.vault.id
   description = "The virtual network configuration ID."
 }
+
 output "name" {
   value       = azurerm_key_vault.vault.name
   description = "The name of the virtual network."
 }
+
 output "resource_group_name" {
   value       = azurerm_key_vault.vault.resource_group_name
   description = "The name of the resource group in which to create the virtual network."
 }
+
 output "location" {
   value       = azurerm_key_vault.vault.location
   description = "The location/region where the virtual network is created."
 }
+
+output "tags" {
+  value       = azurerm_key_vault.vault.tags
+  description = "The tags assigned to the resource."
+}
+
 output "access_policies" {
   value       = azurerm_key_vault.vault.access_policy
   description = "Blocks containing configuration of each access policy."
 }
+
 output "keys" {
   value       = azurerm_key_vault.vault.access_policy
   description = "Blocks containing configuration of each key."
 }
+
 output "secrets" {
   value       = azurerm_key_vault.vault.access_policy
   description = "Blocks containing configuration of each secret."
 }
+
 output "contacts" {
   value       = azurerm_key_vault.vault.contact
   description = "Blocks containing each contact."
-}
-output "tags" {
-  value       = azurerm_key_vault.vault.tags
-  description = "The tags assigned to the resource."
 }
